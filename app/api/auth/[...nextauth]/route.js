2541d4d40f59a08f787fe883470ccfa0e4b9b5cc
MehdiRidmani <mahdiridmani22@gmail.com>
2023-07-13 01:49:32 +0100

    api bugs

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google';
 
 import { connectToDB } from "@utils/database";
+import User from "@models/user";
 
 
 const handler = NextAuth({
@@ -12,19 +13,33 @@ const handler = NextAuth({
         })
     ],
     async session({ session }) {
+        const sessionUser = await User.findOne({
+            email: session.user.email
+        })
+        session.user.id = sessionUser._id.toString();
+
+        return session;
+    },
+    async signIn({ profile }) {
         try {
             await connectToDB();
+            const userExists = await User.finOne({
+                email: profile.email
+            });
 
-
+            if (!userExists) {
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(" ", "").toLowerCase(),
+                    image: profile.picture
+                })
+            }
 
             return true;
         } catch (error) {
             console.log(error);
             return false;
         }
-    },
-    async signIn({ profile }) {
-
     },
 
 })
---
+++ models/user.js
@@ -0,0 +1,23 @@
+import { Schema, model, models } from "mongoose";
+
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        unique: [true, 'Email alredy exists!'],
+        required: [true, 'Email alredy required!'],
+    },
+    username: {
+        type: String,
+        required: [true, 'Username is required!'],
+        match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
+    },
+    image: {
+        type: String,
+    }
+})
+
+
+const User = model.User || model('User', UserSchema);
+
+export default User;
