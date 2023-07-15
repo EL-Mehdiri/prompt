8b456fdee074f9af43708e345a7767071a059711
MehdiRidmani <mahdiridmani22@gmail.com>
2023-07-15 12:21:53 +0100

    api and crud app done!

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,41 +1,61 @@
-'use client';
+"use client";
 
-import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-
 
 import Profile from "@components/Profile";
 
 const MyProfile = () => {
+    const router = useRouter();
     const { data: session } = useSession();
-    const [posts, setPosts] = useState([]);
+
+    const [myPosts, setMyPosts] = useState([]);
+
     useEffect(() => {
-        const fetchPost = async () => {
-            const response = await fetch(`/api/users/${session?.user.id}/post`);
+        const fetchPosts = async () => {
+            const response = await fetch(`/api/users/${session?.user.id}/posts`);
             const data = await response.json();
 
-            setPosts(data);
+            setMyPosts(data);
+        };
+
+        if (session?.user.id) fetchPosts();
+    }, [session?.user.id]);
+
+    const handleEdit = (post) => {
+        router.push(`/update-prompt?id=${post._id}`);
+    };
+
+    const handleDelete = async (post) => {
+        const hasConfirmed = confirm(
+            "Are you sure you want to delete this prompt?"
+        );
+
+        if (hasConfirmed) {
+            try {
+                await fetch(`/api/prompt/${post._id.toString()}`, {
+                    method: "DELETE",
+                });
+
+                const filteredPosts = myPosts.filter((item) => item._id !== post._id);
+
+                setMyPosts(filteredPosts);
+            } catch (error) {
+                console.log(error);
+            }
         }
-        if (session?.user.id) fetchPost();
-    }, [])
-    const handleEdite = () => {
-
-    }
-
-    const handleDelete = async () => {
-
-    }
+    };
 
     return (
-        <Profile name='My'
-            desc='Welcome to your presonalized profile page'
-            data={posts}
-            handleEdite={handleEdite}
+        <Profile
+            name='My'
+            desc='Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination'
+            data={myPosts}
+            handleEdit={handleEdit}
             handleDelete={handleDelete}
-
         />
-    )
-}
+    );
+};
 
-export default MyProfile
+export default MyProfile;
